c5b38fe9599f4963aac828eb74293335faa88fcd
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2022-11-21 20:25:17 +0700

    Create example page ecommerce

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Hero from "./components/Hero/Hero";
+import Slider from "./components/Slider/Slider";
+import Virtual from "./components/Virtual/Virtual";
+import Products from "./components/Products/Products";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <span>Subscribe Zainkeepscode</span>
+      <Header />
+      <Hero />
+      <Slider />
+      <Virtual />
+      <Products />
+      <Testimonials/>
+      <Footer/>
     </div>
   );
 }
